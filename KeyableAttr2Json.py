12c4e6fba409b536192341d91209f297d1cf1cec
Ananya <iyerananya20@gmail.com>
2021-02-20 20:15:10 +0530

    initial commit

---
+++ KeyableAttr2Json.py
@@ -0,0 +1,25 @@
+import pymel.core as pm
+import json
+import os, sys
+obj_sets = [i for i in pm.selected() if i.name() == "ControlSet" and i.nodeType()=="objectSet"]
+if not obj_sets:
+    raise
+main_dict = {}
+for obj_set in obj_sets:
+    obj_set_children = pm.sets(obj_set, q=1)
+    for item in obj_set_children:
+        item_attr = pm.listAttr(item, keyable=1)
+        if not item_attr:continue
+        main_dict[item.name()]= {}
+        for each in item_attr:
+            val = item.attr(each).get()
+            main_dict[item.name()][each]= val
+
+if os.path.exists('C:/temp'):
+    path = 'c:/temp/'
+else:
+    path = raw_input()
+
+if path:
+    with open('{}/out.json'.format(path), 'w') as writer:
+        json.dump(main_dict, writer, indent=2)
